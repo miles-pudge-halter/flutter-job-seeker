7e330f99cb4f5e9846a28513e4e1e04dcc6ad3de
Annt Phyo Thwin <blacknight.mm@gmail.com>
2022-07-05 11:27:51 +0700

    feat(login-ui): Login entry page UI

---
+++ lib/feature/login/view/login_page.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:job_seeker/Constants.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            const CircleAvatar(
+              radius: 150,
+              backgroundImage: NetworkImage(
+                  'https://images.unsplash.com/photo-1518306896927-d58c59c07b82?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTIxfHxzZWFyY2hpbmclMjB3aXRoJTIwYmlub2N1bGFyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Text(
+                'Find Your Dream Job With Us',
+                style: Theme.of(context).textTheme.headline1,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Text(
+              'Apply your best job and best locations',
+              style: Theme.of(context).textTheme.bodyText2?.copyWith(
+                color: ThemeColor().darkGrey,
+              ),
+            ),
+            const SizedBox(height: 30),
+            _buildLoginButton('Login with Email', false, context),
+            const SizedBox(height: 20),
+            _buildLoginButton('Login with Google', true, context),
+            const SizedBox(height: 40),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLoginButton(
+      String title, bool isSocialLogin, BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Container(
+        decoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          borderRadius: const BorderRadius.all(Radius.circular(20)),
+          color: Colors.grey.shade200,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  isSocialLogin
+                      ? Image.network(
+                          'https://myplaceloungechiangmai.com/wp-content/uploads/2015/02/google-logo-icon-PNG-Transparent-Background.png',
+                          width: 30,
+                          height: 30,
+                        )
+                      : const Icon(
+                          Icons.alternate_email_rounded,
+                          size: 30,
+                          color: Colors.blue,
+                        ),
+                  const SizedBox(width: 10),
+                  Text(title),
+                ],
+              ),
+              Container(
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  borderRadius: const BorderRadius.all(Radius.circular(8)),
+                  color: ThemeColor().blue,
+                ),
+                child: const Icon(
+                  Icons.arrow_forward_rounded,
+                  color: Colors.white,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:job_seeker/Constants.dart';
+import 'package:job_seeker/feature/login/view/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,24 +11,30 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+          primaryColor: ThemeColor().blue,
+          fontFamily: GoogleFonts.lato().fontFamily,
+          textTheme: TextTheme(
+            headline1: const TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+            bodyText2: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+              color: Colors.grey.shade700,
+            )
+          )),
+      getPages: [
+        GetPage(name: '/login', page: () => LoginPage()),
+      ],
+      initialRoute: '/login',
     );
   }
 }
