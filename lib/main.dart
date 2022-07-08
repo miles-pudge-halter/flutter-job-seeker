cb430dded407deb5ee3158e118e9b6edc38b3b94
Annt Phyo Thwin <blacknight.mm@gmail.com>
2022-07-08 11:28:36 +0700

    feat(firebase-auth): Base firebase configs setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -10,7 +11,13 @@ import 'package:job_seeker/feature/login/view/login_entry_page.dart';
 import 'package:job_seeker/feature/login/view/login_page.dart';
 import 'package:job_seeker/feature/login/view/signup_page.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
