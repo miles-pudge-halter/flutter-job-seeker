b85d15157cabae94461766328b5b25ef0ba871e4
Annt Phyo Thwin <blacknight.mm@gmail.com>
2022-07-11 15:53:41 +0700

    feat(jobs-api-integration): Bind job details page with api

--- lib/feature/job_details/view/job_details_page.dart
+++ lib/feature/job_details/view/job_details_page.dart
@@ -1,27 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:get/get.dart';
+import 'package:job_seeker/Constants.dart';
 import 'package:job_seeker/data/models/job_model.dart';
 import 'package:job_seeker/feature/shared/buttons.dart';
 
 class JobDetailsPage extends StatelessWidget {
-  final JobModel job = JobModel(
-    id: '1',
-    role: 'Android Engineer',
-    companyName: 'Google',
-    datePosted: '1/1/1999',
-    keywords: ['keywords'],
-    remote: false,
-    logo:
-        'https://myplaceloungechiangmai.com/wp-content/uploads/2015/02/google-logo-icon-PNG-Transparent-Background.png',
-    text:
-        '<p>Cube is a data operating system that enables organizations to easily automate their data work.  We are looking to add a Senior Product Designer to our dynamic team who would be an important part of growing our business.</p>\n<p>The Senior Product Designer will contribute in the following areas:</p>\n<h4>Responsibilities</h4>\n<ul>\n<li><p>Participate in agile sprint planning sessions and daily standups where the team aligns on priorities</p>\n</li>\n<li><p>Transform product manager specs into UX/UI designs</p>\n</li>\n<li><p>Define requirements and designs in coordination with product managers, engineers and users</p>\n</li>\n<li><p>Create wireframes, user flows, and everything from low- to high- fidelity designs</p>\n</li>\n<li><p>Drive redesign of existing website and software platform</p>\n</li>\n<li><p>Conduct user testing and research to understand customer behavior and expectations</p>\n</li>\n<li><p>Proactively define and deliver industry-leading experiences for our customers</p>\n</li>\n<li><p>Engage with engineers and product managers for thorough QA at the end of the delivery process</p>\n</li>\n<li><p>Play a part in maintaining the brand\'s consistency across all web designs and marketing materials.</p>\n</li>\n</ul>\n<h4>Requirements:</h4>\n<ul>\n<li><p>Experience with UX/UI design for web applications</p>\n</li>\n<li><p>Strong technical skills and experience working with popular graphic design programs (Photoshop, Illustrator, Figma, Sketch, etc)</p>\n</li>\n<li><p>Familiarity with pattern library + atomic design principles</p>\n</li>\n<li><p>Strong interest in data analytics and SaaS platforms</p>\n</li>\n<li><p>Appetite for working in a fast-paced, demanding environment with the potential for exponential growth</p>\n</li>\n<li><p>Desire to play a direct role in growing our company and improving our product</p>\n</li>\n<li><p>Ability to take on significant responsibility starting day one and be exposed to multiple aspects of the product / design lifecycle</p>\n</li>\n<li><p>Talent for solving design challenges in the data and analytics space</p>\n</li>\n<li><p>Prior exposure to data science and statistical modeling products a plus</p>\n</li>\n</ul>\n<p>We are looking to fill the Designer role quickly given recent growth.  In addition to base salary, this role has the opportunity to participate in our stock option plan.</p>\n',
-  );
+  late JobModel job;
 
   JobDetailsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    job = Get.arguments['job'];
     return Scaffold(
       body: SafeArea(
         child: Column(
@@ -73,7 +64,15 @@ class JobDetailsPage extends StatelessWidget {
                           job.logo ?? '',
                           width: 100,
                           height: 100,
-                          fit: BoxFit.cover,
+                          fit: BoxFit.contain,
+                          errorBuilder: (BuildContext context, Object exception,
+                              StackTrace? stackTrace) {
+                            return Icon(
+                              Icons.location_city,
+                              size: 100,
+                              color: Colors.grey.shade400,
+                            );
+                          },
                         ),
                       ),
                       const SizedBox(height: 20),
@@ -93,6 +92,8 @@ class JobDetailsPage extends StatelessWidget {
                         ),
                       ),
                       SizedBox(height: 20),
+                      _buildSkills(job.keywords, context),
+                      SizedBox(height: 20),
                       _buildJobDescription(job.text ?? '', context),
                     ],
                   ),
@@ -110,9 +111,12 @@ class JobDetailsPage extends StatelessWidget {
                       flex: 4,
                       child: Container(
                         height: 55,
-                        child: NFQPrimaryButton('Apply This Job', () {
-                          Get.toNamed('/job/apply');
-                        },),
+                        child: NFQPrimaryButton(
+                          'Apply This Job',
+                          () {
+                            Get.toNamed('/job/apply');
+                          },
+                        ),
                       ),
                     ),
                     SizedBox(width: 20),
@@ -133,6 +137,39 @@ class JobDetailsPage extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+
+  _buildSkills(List<String> keywords, BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Skills',
+            style: Theme.of(context).textTheme.subtitle1,
+          ),
+          SizedBox(height: 10),
+          Wrap(
+            spacing: 8,
+            runSpacing: 2,
+            children: keywords.map((skill) => _buildSkillChip(skill)).toList(),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSkillChip(String title) {
+    return Chip(
+      label: Text(
+        title,
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+      backgroundColor: ThemeColor().blue,
     );
   }
 
